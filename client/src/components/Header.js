81abbf078a2ecb8e135991994aa2ed1b90341f4b
Siyu Chen <siyuchen1@ufl.edu>
2021-07-01 05:36:12 -0400

    changes name

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -8,7 +8,7 @@ const Header = () => {
       <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
         <Container>
           <LinkContainer to='/'>
-            <Navbar.Brand>Shop</Navbar.Brand>
+            <Navbar.Brand>Shoppingfy</Navbar.Brand>
           </LinkContainer>
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
           <Navbar.Collapse id='basic-navbar-nav'>
