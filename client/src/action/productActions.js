695bcf82c8b3a8b2d0136b8b4ad3c049e080a903
Siyu Chen <siyuchen1@ufl.edu>
2021-07-01 14:27:02 -0400

    bring redux to homescreen

--- client/src/action/productActions.js
+++ client/src/action/productActions.js
@@ -5,7 +5,7 @@ import {
 } from '../constants/productConstants';
 import axios from 'axios';
 
-export default listProducts = () => async (dispatch) => {
+export const listProducts = () => async (dispatch) => {
   try {
     dispatch({ type: PRODUCT_LIST_REQUEST });
 
