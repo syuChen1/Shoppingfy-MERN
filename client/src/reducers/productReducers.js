0707b6137ad206a064c289fc2da1eae973a8888f
Siyu Chen <siyuchen1@ufl.edu>
2021-07-02 11:42:53 -0400

    Profile Screen & Get User Details

--- client/src/reducers/productReducers.js
+++ client/src/reducers/productReducers.js
@@ -10,9 +10,9 @@ import {
 export const productListReducer = (state = { products: [] }, action) => {
   switch (action.type) {
     case PRODUCT_LIST_REQUEST:
-      return { locading: true, products: [] };
+      return { loading: true, products: [] };
     case PRODUCT_LIST_SUCCESS:
-      return { locading: false, products: action.payload };
+      return { loading: false, products: action.payload };
     case PRODUCT_LIST_FAIL:
       return { loading: false, error: action.payload };
     default:
--- client/src/reducers/userReducers.js
+++ client/src/reducers/userReducers.js
@@ -6,14 +6,17 @@ import {
   USER_REGISTER_FAIL,
   USER_REGISTER_REQUEST,
   USER_REGISTER_SUCCESS,
+  USER_DETAILS_REQUEST,
+  USER_DETAILS_SUCCESS,
+  USER_DETAILS_FAIL,
 } from '../constants/userConstants';
 
 export const userLoginReducer = (state = {}, action) => {
   switch (action.type) {
     case USER_LOGIN_REQUEST:
-      return { locading: true };
+      return { loading: true };
     case USER_LOGIN_SUCCESS:
-      return { locading: false, userInfo: action.payload };
+      return { loading: false, userInfo: action.payload };
     case USER_LOGIN_FAIL:
       return { loading: false, error: action.payload };
     case USER_LOGOUT:
@@ -26,10 +29,23 @@ export const userLoginReducer = (state = {}, action) => {
 export const userRegisterReducer = (state = {}, action) => {
   switch (action.type) {
     case USER_REGISTER_REQUEST:
-      return { locading: true };
+      return { loading: true };
     case USER_REGISTER_SUCCESS:
-      return { locading: false, userInfo: action.payload };
+      return { loading: false, user: action.payload };
     case USER_REGISTER_FAIL:
+      return { loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
+
+export const userDetailsReducer = (state = { user: {} }, action) => {
+  switch (action.type) {
+    case USER_DETAILS_REQUEST:
+      return { ...state, loading: true };
+    case USER_DETAILS_SUCCESS:
+      return { loading: false, user: action.payload };
+    case USER_DETAILS_FAIL:
       return { loading: false, error: action.payload };
     default:
       return state;
