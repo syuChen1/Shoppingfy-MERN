d0370a643464d213b31b05214a3b05b1616d41bf
Siyu Chen <siyuchen1@ufl.edu>
2021-07-02 09:36:39 -0400

    show user in navbar and log out

--- client/src/action/userActions.js
+++ client/src/action/userActions.js
@@ -43,3 +43,8 @@ export const login = (email, password) => async (dispatch) => {
     });
   }
 };
+
+export const logout = () => async (dispatch) => {
+  localStorage.removeItem('userInfo');
+  dispatch({ type: USER_LOGOUT });
+};
