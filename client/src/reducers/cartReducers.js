319a79c5347ff5d33a98b22d9bb5ab643c6141c2
Siyu Chen <siyuchen1@ufl.edu>
2021-07-03 00:18:05 -0400

    payment screen

--- client/src/reducers/cartReducers.js
+++ client/src/reducers/cartReducers.js
@@ -2,6 +2,7 @@ import {
   CART_ADD_ITEM,
   CART_REMOVE_ITEM,
   CART_SAVE_SHIPPING_ADDRESS,
+  CART_SAVE_PAYMENT_METHOD,
 } from '../constants/cartConstants';
 
 export const cartReducer = (
@@ -27,16 +28,25 @@ export const cartReducer = (
           cartItems: [...state.cartItems, item],
         };
       }
+
     case CART_REMOVE_ITEM:
       return {
         ...state,
         cartItems: state.cartItems.filter((x) => x.product !== action.payload),
       };
+
     case CART_SAVE_SHIPPING_ADDRESS:
       return {
         ...state,
         shippingAddress: action.payload,
       };
+
+    case CART_SAVE_PAYMENT_METHOD:
+      return {
+        ...state,
+        paymentMethod: action.payload,
+      };
+
     default:
       return state;
   }
---
+++ client/src/screens/PlaceOrderScreen.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const PlaceOrderScreen = () => {
+  return <div></div>;
+};
+
+export default PlaceOrderScreen;
