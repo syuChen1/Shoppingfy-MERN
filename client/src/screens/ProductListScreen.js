409d61431cd4d9786f80fe7b497ddab0ed6f4a08
Siyu Chen <siyuchen1@ufl.edu>
2021-07-08 15:24:24 -0400

    admin delete product

--- client/src/screens/ProductListScreen.js
+++ client/src/screens/ProductListScreen.js
@@ -4,13 +4,20 @@ import { Table, Button, Row, Col } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
 import Message from '../components/Message';
 import Loader from '../components/Loader';
-import { listProducts } from '../action/productActions';
+import { listProducts, deleteProduct } from '../action/productActions';
 
 const ProductListScreen = ({ history, match }) => {
   const dispatch = useDispatch();
 
   const productList = useSelector((state) => state.productList);
   const { loading, error, products } = productList;
+
+  const productDelete = useSelector((state) => state.productDelete);
+  const {
+    loading: loadingDelete,
+    error: errorDelete,
+    success: successDelete,
+  } = productDelete;
 
   const userLogin = useSelector((state) => state.userLogin);
   const { userInfo } = userLogin;
@@ -20,11 +27,11 @@ const ProductListScreen = ({ history, match }) => {
     else {
       history.push('/login');
     }
-  }, [dispatch, history, userInfo]);
+  }, [dispatch, history, userInfo, successDelete]);
 
   const deleteHandler = (id) => {
     if (window.confirm('Are you sure')) {
-      //delete product
+      dispatch(deleteProduct(id));
     }
   };
 
@@ -44,6 +51,8 @@ const ProductListScreen = ({ history, match }) => {
           </Button>
         </Col>
       </Row>
+      {loadingDelete && <Loader />}
+      {errorDelete && <Message variant='danger'>{errorDelete}</Message>}
       {loading ? (
         <Loader />
       ) : error ? (
